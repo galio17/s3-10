b0feaaf5e93288343a5d3c51d2f2ecc3ad3f8288
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-04-12 11:03:46 -0300

    code improvement

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 function positions(firstPlace, secondPlace, lastPlace) {
-    function templatePodium(element, index) {
-        console.log(`${index+1}ª - Colocado "${element}"`);
-    }
-    
     let podium = [firstPlace, secondPlace, lastPlace];
     let danielPosition = podium.indexOf('Daniel');
-
+    
     if(danielPosition > 0) {
         let positionRival = podium[danielPosition-1];
         podium[danielPosition-1] = 'Daniel';
         podium[danielPosition] = positionRival;
     }
-
-    return podium.forEach(templatePodium);
+    
+    return podium;
 }
 
-console.log('Caso Daniel terminando em 1º');
-positions('Daniel', 'Manoel', 'Rafael');
+function templatePodium(competitor, position) {
+    console.log(`${position+1}ª - Colocado "${competitor}"`);
+}
 
-console.log('Caso Daniel terminando em 2º');
-positions('Rafael', 'Daniel', 'Manoel');
+let podiumCase1 = positions('Daniel', 'Manoel', 'Rafael');
+let podiumCase2 = positions('Rafael', 'Daniel', 'Manoel');
+let podiumCase3 = positions('Manoel', 'Rafael', 'Daniel');
 
-console.log('Caso Daniel terminando em 3º');
-positions('Manoel', 'Rafael', 'Daniel');
+console.log('Caso 1 - Daniel em 1º')
+podiumCase1.forEach(templatePodium);
+console.log('Caso 2 - Daniel em 2º')
+podiumCase2.forEach(templatePodium);
+console.log('Caso 3 - Daniel em 3º')
+podiumCase3.forEach(templatePodium);
